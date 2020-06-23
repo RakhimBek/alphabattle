e51467b65cb19d3d71ec6e32b6dc829d033311fc
Rakhimbekov R. R <rrakhimbekov@gmail.com>
2020-06-23 21:04:05 +0600

    gson dependency

--- starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/controller/HelloController.java
+++ starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/controller/HelloController.java
@@ -1,17 +1,25 @@
 package ru.raimbek.rakhimbekov.alphabattle.controller;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.raimbek.rakhimbekov.alphabattle.dto.Status;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("api")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello", produces = "application/json")
     public String hello() {
-        return "Hello, World!";
+        final Status status = new Status();
+        status.setMessage("Hello, World!");
+
+        return GsonBuilderUtils.gsonBuilderWithBase64EncodedByteArrays()
+                .setPrettyPrinting()
+                .create()
+                .toJson(status);
     }
 }
---
+++ starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/dto/Status.java
@@ -0,0 +1,13 @@
+package ru.raimbek.rakhimbekov.alphabattle.dto;
+
+public class Status {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
