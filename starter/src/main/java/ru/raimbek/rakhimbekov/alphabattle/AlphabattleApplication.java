af022cbfb94dd101597f036480456bb71ff42ed0
Raimbek <rrakhimbekov@gmail.com>
2020-06-23 20:23:37 +0600

    spring init

---
+++ starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/AlphabattleApplication.java
@@ -0,0 +1,13 @@
+package ru.raimbek.rakhimbekov.alphabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphabattleApplication.class, args);
+	}
+
+}
---
+++ starter/src/test/java/ru/raimbek/rakhimbekov/alphabattle/AlphabattleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.raimbek.rakhimbekov.alphabattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
