f9eedd6e53f9e1f6e21a2bd99e48dedad81ed018
Raimbek <rrakhimbekov@gmail.com>
2020-06-27 13:10:37 +0600

    some refactor + ssl false

--- starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/controller/GreetingController.java
+++ starter/src/main/java/ru/raimbek/rakhimbekov/alphabattle/controller/GreetingController.java
@@ -13,31 +13,3 @@ public class GreetingController {
         return String.format("Hello, World(%s)!", request);
     }
 }
-
-/*
-e.g: python3 -m http.server
-client-side:
-
-<html>
-	<head>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.4.0/sockjs.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.js"></script>
-	</head>
-	<body>
-		<script type="text/javascript">
-			var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
-		    stompClient = Stomp.over(socket);
-		    stompClient.connect({}, function (frame) {
-		        console.log('Connected: ' + frame);
-
-		        stompClient.subscribe('/topic/greetings', function (greeting) {
-		            console.log(greeting.body);
-		        });
-		    });
-
-		    stompClient.send("/app/hello", {}, 'Hello, Test Stomp');
-
-		</script>
-	</body>
-</html>
-*/
